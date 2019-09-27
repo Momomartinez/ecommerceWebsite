7046cd54b2e3e46886e066f1418cee6be66d7e3d
gemlagman <gemlagman96@gmail.com>
2019-09-27 02:57:59 -0700

    added routes for about me pages

--- application/src/index.js
+++ application/src/index.js
@@ -1,13 +1,17 @@
-var createError = require('http-errors');
+//var createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const PORT = 3000;
 
 const app = express();
-const aboutRouter = require('../src/routs/aboutRoutes');
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, "public")));
-app.use('/about/', aboutRouter);
+const aboutRouter = require("./routes/aboutRoutes");
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "../public")));
+app.use("/about", aboutRouter);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 app.listen(PORT, () => console.log("server started on port", PORT));
