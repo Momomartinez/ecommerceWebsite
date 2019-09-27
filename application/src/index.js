a6e145e9460604a1ae89b1761ba0416bb6805630
RayaFarshad <rayafarshad@gmail.com>
2019-09-27 00:15:16 -0700

    change the dir path name of the public

--- application/src/index.js
+++ application/src/index.js
@@ -5,6 +5,6 @@ const PORT = 3000;
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => console.log("server started on port", PORT));
