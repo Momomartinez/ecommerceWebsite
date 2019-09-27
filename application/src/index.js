3a22ac1d49945ac1eacb229fe6abc82abfad9ec8
RayaFarshad <rayafarshad@gmail.com>
2019-09-27 00:32:26 -0700

    changes on index.js in order to connect the routes

--- application/src/index.js
+++ application/src/index.js
@@ -1,10 +1,13 @@
+var createError = require('http-errors');
 const express = require("express");
 const path = require("path");
 const PORT = 3000;
 
 const app = express();
+const aboutRouter = require('./src/routes/aboutRoutes');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, "public")));
+app.use('/about/', aboutRouter);
 
 app.listen(PORT, () => console.log("server started on port", PORT));
