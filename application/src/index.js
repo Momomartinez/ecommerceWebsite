13e989a9730a72a2c0e4c4ed742e1d28f1bf68a2
RayaFarshad <rayafarshad@gmail.com>
2019-09-26 23:19:05 -0700

    add ejs express as view engine

--- application/src/index.js
+++ application/src/index.js
@@ -3,7 +3,8 @@ const path = require("path");
 const PORT = 3000;
 
 const app = express();
-
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.listen(PORT, () => console.log("server started on port", PORT));
