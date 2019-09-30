a587d38105ec10432fc66785b455a131c342a0f8
RayaFarshad <rayafarshad@gmail.com>
2019-09-29 21:54:21 -0700

    Change the path

--- application/index.js
+++ application/index.js
@@ -4,10 +4,10 @@ const path = require("path");
 const PORT = 3000;
 
 const app = express();
-const aboutRouter = require('../src/routs/aboutRoutes');
-app.set('views', path.join(__dirname, 'views'));
+const aboutRouter = require('./src/routs/aboutRoutes');
+app.set('views', path.join(__dirname, './src/views'));
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "./public")));
 app.use('/', aboutRouter);
 
 app.listen(PORT, () => console.log("server started on port", PORT));
