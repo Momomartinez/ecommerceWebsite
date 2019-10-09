62994a685145ffabdd4e8f44ca881bd0348ea7f4
Monique <momomartinez90@gmail.com>
2019-10-09 16:22:35 -0700

    made changes to index.ejs for ejs access

--- application/src/index.js
+++ application/src/index.js
@@ -5,13 +5,14 @@ const PORT = 3000;
 
 const app = express();
 const aboutRouter = require("./routes/aboutRoutes");
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, "../public")));
 app.use("/about", aboutRouter);
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  //res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.render('pages/index');
 });
 
 //for testing the mainpage demo
