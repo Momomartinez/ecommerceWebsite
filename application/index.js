4014204a97985eca56724b2b575c71193244a57f
gemlagman <gemlagman96@gmail.com>
2019-10-11 00:41:12 -0700

    fixed typo with routes folder

--- application/index.js
+++ application/index.js
@@ -3,15 +3,14 @@ const path = require("path");
 const PORT = 3000;
 
 const app = express();
-const aboutRouter = require('./src/routs/aboutRoutes');
-app.set('views', path.join(__dirname, './src/views'));
-app.set('view engine', 'ejs');
+const aboutRouter = require("./src/routes/aboutRoutes");
+app.set("views", path.join(__dirname, "./src/views"));
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "./public")));
-app.use('/about', aboutRouter);
+app.use("/about", aboutRouter);
 
-
-app.get('/', (req, res) => {
-  res.render('pages/mainpage');
+app.get("/", (req, res) => {
+  res.render("pages/mainpage");
 });
 
 app.listen(PORT, () => console.log("server started on port", PORT));
