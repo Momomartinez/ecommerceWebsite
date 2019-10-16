440221022603bd7aaa3596d0636666f75db20b10
Monique <momomartinez90@gmail.com>
2019-10-15 18:40:00 -0700

    made ejs dynamic content

--- application/index.js
+++ application/index.js
@@ -1,16 +1,22 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const PORT = 3000;
 
 const app = express();
-const aboutRouter = require("./src/routes/aboutRoutes");
-app.set("views", path.join(__dirname, "./src/views"));
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "./public")));
-app.use("/about", aboutRouter);
+const aboutRouter = require('./src/routes/aboutRoutes');
+app.set('views', path.join(__dirname, './src/views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, './public')));
+app.use('/about', aboutRouter);
 
-app.get("/", (req, res) => {
-  res.render("pages/mainpage");
+app.get('/', (req, res) => {
+  var items = [
+    { itemImage: 'images/chair.jpg', itemName: 'chair', itemPrice: '$100.00' },
+    { itemImage: 'images/face.jpg', itemName: 'chair', itemPrice: '$80.00' },
+    { itemImage: 'images/chair.jpg', itemName: 'chair', itemPrice: '$20.00' },
+    { itemImage: 'images/default.jpg', itemName: 'chair', itemPrice: '$30.00' }
+  ];
+  res.render('pages/mainpage', { cards: items });
 });
 
-app.listen(PORT, () => console.log("server started on port", PORT));
+app.listen(PORT, () => console.log('server started on port', PORT));
