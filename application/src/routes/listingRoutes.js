d03ce3cae8ccf6b599afecbef500ebc7c1690427
RayaFarshad <rayafarshad@gmail.com>
2019-10-22 15:22:46 -0700

    use await for geting the queries

--- application/src/routes/listingRoutes.js
+++ application/src/routes/listingRoutes.js
@@ -10,46 +10,53 @@ const listing_type = {
     Other: 4,
 };
 
+async function listingGet(req, res){
+    const [type_rows, type_fields] = await db.execute('SELECT * FROM listing_type');
+    const [listing_rows, listing_fields] = await db.execute('SELECT listing.id, listing.title, listing.price, listing.desc, listing.image FROM listing order by listing.id');
+
+    res.render('pages/mainpage', {
+        cards: type_rows,
+        listings: listing_rows,
+        body: req.body,
+    });
+    console.log(type_rows);
+    console.log("hello")
+    console.log(listing_rows);
+}
+
 router.route('/')
     .get((req, res) => {
-        console.log("hggggggg");
-        db.query(' SELECT listing.id, listing.title, listing.price, listing.desc, listing.image FROM listing order by listing.id ')
-            .then(([results, fields]) => {
-                res.render('pages/mainpage', {
-                    cards: results,
-                    body: req.body,
-                });
-            });
+        listingGet(req, res);
     })
 
-    .get((req, res) => {
-    console.log("fffffff");
-    let sql = ' SELECT * FROM listing ';
-    const conditions = [];
-    console.log(req.body);
-    console.log("fffffff");
-    console.log(req.body.listing_type_id);
-
-    // listing type id - temp solution
-    if (req.body.listing_type_id !== '') {
-        const id = listing_type[req.body.listing_type_id];
-        conditions.push(`listing.listing_type_id = ${id}`);
-    }
-    if (conditions.length !== 0) {
-        sql += ' AND ( ';
-        sql += conditions.join(' AND ');
-        sql += ' ) ';
-
-    }
-
-    db.query(sql)
-        .then(([results, _]) => {
-            res.render('pages/index', {
-                body: req.body,
-                cards: results,
-            });
-        });
-});
+//     .get((req, res) => {
+//     console.log("fffffff");
+//     let sql = ' SELECT * FROM listing ';
+//     const conditions = [];
+//     console.log(req.body);
+//     console.log("fffffff");
+//     console.log(req.body.listing_type_id);
+//
+//     // listing type id - temp solution
+//     if (req.body.listing_type_id !== '') {
+//         const id = listing_type[req.body.listing_type_id];
+//         conditions.push(`listing.listing_type_id = ${id}`);
+//     }
+//     if (conditions.length !== 0) {
+//         sql += ' AND ( ';
+//         sql += conditions.join(' AND ');
+//         sql += ' ) ';
+//
+//     }
+//
+//     db.query(sql)
+//         .then(([results, _]) => {
+//             res.render('pages/index', {
+//                 body: req.body,
+//                 cards: results,
+//             });
+//         });
+// });
 
 router.route('/:id(\\d+)')
     .get((req, res) => {
