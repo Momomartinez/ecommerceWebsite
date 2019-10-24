0f7a42ef2e0dbedbdc18cc79b17d8eec1da54e3a
gemlagman <gemlagman96@gmail.com>
2019-10-24 04:11:23 -0700

    added routes for search but still need to modify

--- application/index.js
+++ application/index.js
@@ -1,21 +1,22 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const PORT = 3000;
 
 const app = express();
 
-app.set('views', path.join(__dirname, './src/views'));
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
-const bodyParser = require('body-parser');
+app.set("views", path.join(__dirname, "./src/views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
-const morgan = require('morgan');
-app.use(morgan('tiny'));
-const aboutRouter = require('./src/routes/aboutRoutes');
-const listingRouter = require('./src/routes/listingRoutes');
+app.use(bodyParser.json());
+const morgan = require("morgan");
+app.use(morgan("tiny"));
+const aboutRouter = require("./src/routes/aboutRoutes");
+const listingRouter = require("./src/routes/listingRoutes");
 
-app.use('/about', aboutRouter);
-app.use('/', listingRouter);
+app.use("/about", aboutRouter);
+app.use("/", listingRouter);
 
 // app.get('/', (req, res) => {
 //   var items = [
@@ -35,4 +36,4 @@ app.use('/', listingRouter);
 //   res.render('pages/mainpage', { cards: items });
 // });
 
-app.listen(PORT, () => console.log('server started on port', PORT));
+app.listen(PORT, () => console.log("server started on port", PORT));
--- application/src/routes/listingRoutes.js
+++ application/src/routes/listingRoutes.js
@@ -1,45 +1,141 @@
-
-const express = require('express');
-const db = require('../models/database.js');
+const express = require("express");
+const db = require("../models/database.js");
 const router = express.Router();
 
+// async function listingGet(req, res) {
+//   const [type_rows, type_fields] = await db.execute(
+//     "SELECT * FROM listing_type"
+//   );
+//   // const [listing_rows, listing_fields] = await db.execute('SELECT listing.id, listing.title, listing.price, listing.desc, listing.image FROM listing order by listing.id');
+//   res.render("pages/mainpage", {
+//     // types: type_rows,
+//     cards: type_rows,
+//     body: req.body
+//   });
+//   console.log("this is the cards row " + type_rows);
+// }
 
-async function listingGet(req, res){
-    const [type_rows, type_fields] = await db.execute('SELECT * FROM listing_type');
-    // const [listing_rows, listing_fields] = await db.execute('SELECT listing.id, listing.title, listing.price, listing.desc, listing.image FROM listing order by listing.id');
-    res.render('pages/mainpage', {
-        // types: type_rows,
-        cards: type_rows,
-        body: req.body,
-    });
-    console.log("this is the cards row " +type_rows);
+// router.route("/").get((req, res) => {
+//   listingGet(req, res);
+// });
+
+router.get("/categories", (req, res) => {
+  db.query("SELECT * FROM listing_type", (err, rows) => {
+    if (err) throw err;
+    return res.json(rows);
+  });
+});
+
+async function search(req, res, next) {
+  var searchTerm = req.query.search;
+  var categoryName = req.query.category;
+  var categoryId = 0;
+
+  var catquery =
+    `SELECT * FROM listing_type WHERE name = '` + categoryName + `'`;
+  await db.query(catquery, (err, results) => {
+    // if (err) throw err;
+    if (results.length <= 0) {
+      req.searchResult = "";
+      console.log("This is the result" + results);
+      next();
+    } else {
+      categoryId = results[0].id;
+      console.log("This is categoryId: " + categoryId);
+      console.log(results);
+      let query = "SELECT * FROM listing";
+      if (searchTerm != "" && categoryId != 0) {
+        query =
+          `SELECT * FROM listing WHERE listing_type_id = '` +
+          categoryId +
+          `' AND (title LIKE '%` +
+          searchTerm +
+          `%' OR description LIKE '%` +
+          searchTerm +
+          `%')`;
+      } else if (searchTerm != "" && categoryId == 0) {
+        query =
+          `SELECT * FROM listing WHERE (title LIKE '%` +
+          searchTerm +
+          `%' OR description LIKE '%` +
+          searchTerm +
+          `%')`;
+      } else if (searchTerm == "" && categoryId != 0) {
+        query =
+          `SELECT * FROM listing WHERE listing_type_id = '` + categoryId + `'`;
+      }
+
+      console.log(query);
+      db.query(query, (err, result) => {
+        if (err) {
+          req.searchResult = "";
+          console.log(result);
+          next();
+        }
+        req.searchResult = result;
+        next();
+      });
+    }
+  });
 }
 
-router.route('/')
-    .get((req, res) => {
-        listingGet(req, res);
-    })
+//   let query = "SELECT * FROM listing";
+//   if (searchTerm != "" && categoryId != 0) {
+//     query =
+//       `SELECT * FROM listing WHERE listing_type_id = '` +
+//       categoryId +
+//       `' AND (title LIKE '%` +
+//       searchTerm +
+//       `%' OR description LIKE '%` +
+//       searchTerm +
+//       `%')`;
+//   } else if (searchTerm != "" && categoryId == 0) {
+//     query =
+//       `SELECT * FROM listing WHERE (title LIKE '%` +
+//       searchTerm +
+//       `%' OR description LIKE '%` +
+//       searchTerm +
+//       `%')`;
+//   } else if (searchTerm == "" && categoryId != 0) {
+//     query =
+//       `SELECT * FROM listing WHERE listing_type_id = '` + categoryId + `'`;
+//   }
 
+//   console.log(query);
+//   db.query(query, (err, result) => {
+//     if (err) {
+//       req.searchResult = "";
+//       console.log(result);
+//       next();
+//     }
+//     req.searchResult = result;
+//     next();
+//   });
 
-router.route('/:id(\\d+)')
-    .get((req, res) => {
-        console.log("the id is: "+req.params.id);
-        let sql = 'SELECT * FROM listing WHERE listing_type_id = ?';
-        db.query(sql , req.params.id).then(([results, fields]) => {
-            // if (!results || results.length !== 1) {
-            //     res.status(404).send('404 - Page Not found');
-            //     return;
-            // }
-            res.render('pages/mainpage', { cards: results});
-            console.log(req.params.id);
-            console.log("hhhhhhh");
-            console.log(results);
-            console.log(sql);
-            console.log(req.params.id);
-        });
-    });
+router.get("/search", search, (req, res) => {
+  var searchResult = req.searchResult;
+  res.render("pages/mainpage", {
+    cards: searchResult
+  });
 
+  //res.json(searchResult);
+});
 
-
+// router.route("/:id(\\d+)").get((req, res) => {
+//   console.log("the id is: " + req.params.id);
+//   let sql = "SELECT * FROM listing WHERE listing_type_id = ?";
+//   db.query(sql, req.params.id).then(([results, fields]) => {
+//     // if (!results || results.length !== 1) {
+//     //     res.status(404).send('404 - Page Not found');
+//     //     return;
+//     // }
+//     res.render("pages/mainpage", { cards: results });
+//     console.log(req.params.id);
+//     console.log("hhhhhhh");
+//     console.log(results);
+//     console.log(sql);
+//     console.log(req.params.id);
+//   });
+// });
 
 module.exports = router;
