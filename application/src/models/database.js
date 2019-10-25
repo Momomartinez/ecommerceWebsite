d5c0d9ff5dc07d2c671b54e3433e7f08c6c16e84
gemlagman <gemlagman96@gmail.com>
2019-10-25 00:31:03 -0700

    added functionality to search button

--- application/src/models/database.js
+++ application/src/models/database.js
@@ -1,25 +1,19 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-    host: '142.44.170.121',
-    user: 'root',
-    password: '6&rFzI70oM*',
-    database: 'test_raya',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: "142.44.170.121",
+  user: "root",
+  password: "6&rFzI70oM*",
+  database: "test_raya",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-// pool.connect((err) => {
-//     if(err) throw err;
-//     console.log('My database is connected!');
-//     //pool.query('test_raya');
-//
-// })
-
-
-
-
-
+pool.getConnection(err => {
+  if (err) throw err;
+  console.log("My database is connected!");
+  //pool.query('test_raya');
+});
 
 module.exports = pool;
