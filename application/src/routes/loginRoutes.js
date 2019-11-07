cae423e6aec016038adbba29f1be838b31da7fec
RayaFarshad <rayafarshad@gmail.com>
2019-11-07 15:56:16 -0800

    test with test11_db

--- application/src/routes/loginRoutes.js
+++ application/src/routes/loginRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const db = require("../models/database.js");
 const { check, validationResult } = require('express-validator');
+
+
 
 
 router.get('/login', function (req, res, next) {
@@ -11,17 +14,20 @@ router.post("/register", function(req, res, next) {
     var name = req.body.name;
     var email = req.body.username;
     var password = req.body.password;
-    console.log(name);
-    console.log(email);
-   
-
     //check vallidation
-
-
     // var errors = req.validationErrors();
-
-
 });
+
+// async function checkValidation(req, res, next) {
+//     await db.query("SELECT * FROM listing_type", (err, categories) => {
+//         if (err) throw err;
+//         //console.log(categories);
+//         req.categoriesList = categories;
+//         next();
+//     });
+// }
+
+
 
 
 module.exports = router;
