a3ec49a6a6cdd908947b8f4ef9e5a24636d75fef
Monique <momomartinez90@gmail.com>
2019-11-06 18:00:43 -0800

    added login/sigun modal w/ bootstrap

--- application/index.js
+++ application/index.js
@@ -1,21 +1,25 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const PORT = 3000;
 
 const app = express();
 
-app.set("views", path.join(__dirname, "./src/views"));
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
-const bodyParser = require("body-parser");
+app.set('views', path.join(__dirname, './src/views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const morgan = require("morgan");
-app.use(morgan("tiny"));
-const aboutRouter = require("./src/routes/aboutRoutes");
-const listingRouter = require("./src/routes/listingRoutes");
+const morgan = require('morgan');
+app.use(morgan('tiny'));
+const aboutRouter = require('./src/routes/aboutRoutes');
+const listingRouter = require('./src/routes/listingRoutes');
 
-app.use("/about", aboutRouter);
-app.use("/", listingRouter);
+app.use('/about', aboutRouter);
+app.use('/', listingRouter);
 
-app.listen(PORT, () => console.log("server started on port", PORT));
+app.get('/login', function(req, res, next) {
+  res.render('partials/login', {});
+});
+
+app.listen(PORT, () => console.log('server started on port', PORT));
