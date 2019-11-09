fbab0696babdd6f52bd3642faa594e423920fc45
RayaFarshad <rayafarshad@gmail.com>
2019-11-09 15:38:00 -0800

    Update loginRoutes.js

--- application/src/routes/loginRoutes.js
+++ application/src/routes/loginRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const db = require("../models/database.js");
 const { User } = require('../models/user.js');
 const { check, validationResult } = require('express-validator');
 
